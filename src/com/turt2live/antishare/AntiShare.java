380fda750f7aa7e89e03f6aeea94f57ad2ba2f74
Travis Ralston <travpc@gmail.com>
2012-02-20 18:25:48 -0700

    Added test for developing (will not be in release)

--- src/com/turt2live/antishare/AntiShare.java
+++ src/com/turt2live/antishare/AntiShare.java
@@ -2,16 +2,16 @@ package com.turt2live.antishare;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.feildmaster.lib.configuration.PluginWrapper;
 import com.turt2live.antishare.SQL.SQLManager;
 
-// TODO: Ensure use of ASUtils.sendToPlayer();
-// TODO: Add admin channel, for reporting events
-// TODO: ChannelChat implementation for above
 public class AntiShare extends PluginWrapper {
 
 	private ASConfig config;
@@ -36,6 +36,22 @@ public class AntiShare extends PluginWrapper {
 			log.info("AntiShare Reloaded.");
 			ASUtils.sendToPlayer(sender, ChatColor.GREEN + "AntiShare Reloaded.");
 			return true;
+		}else if(cmd.equalsIgnoreCase("astest")){
+			new Thread(new Runnable(){
+				@Override
+				public void run(){
+					while (true){
+						for(Player p : Bukkit.getOnlinePlayers()){
+							if(p.getGameMode() == GameMode.SURVIVAL){
+								p.setGameMode(GameMode.CREATIVE);
+							}else{
+								p.setGameMode(GameMode.SURVIVAL);
+							}
+						}
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lag");
+					}
+				}
+			}).start();
 		}
 		return false;
 	}
