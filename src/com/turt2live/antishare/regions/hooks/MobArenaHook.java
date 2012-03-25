59132659899d3e0a64a6b59424bb1407b2183037
Travis Ralston <travpc@gmail.com>
2012-03-25 03:00:04 -0600

    Updated a huge note-to-self

--- src/com/turt2live/antishare/regions/hooks/MobArenaHook.java
+++ src/com/turt2live/antishare/regions/hooks/MobArenaHook.java
@@ -34,8 +34,18 @@ public class MobArenaHook implements Hook {
 		if(!hasMobArena){
 			return false;
 		}
-		//MobArenaHandler handle = new MobArenaHandler();
+		// Can't really do anything here due to lack of an API,
+		// although the ASRegion alertEntry() has a
+		// region check in it to ensure that if the player is 
+		// in a MobArena they will not be tortured by bugs.
+		// This fixes the issue first discovered where
+		// if a player enters an ovrlapping GM Region with a
+		// MA then the GM Region would override (and ignore)
+		// the MA region without further thought. Now AS gives
+		// priority to MA before playing with the player.
 
+		// MobArenaHandler handle = new MobArenaHandler();
+		// MobArena p = (MobArena) mobarena;
 		return false;
 	}
 
