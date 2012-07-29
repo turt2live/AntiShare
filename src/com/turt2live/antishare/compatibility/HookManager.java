52288285f3988e64e49c00bd3ce4bc5fe6132208
Travis Ralston <travpc@gmail.com>
2012-07-28 23:08:10 -0600

    License

--- src/com/turt2live/antishare/compatibility/HookManager.java
+++ src/com/turt2live/antishare/compatibility/HookManager.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 turt2live (Travis Ralston).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     turt2live (Travis Ralston) - initial API and implementation
+ ******************************************************************************/
 package com.turt2live.antishare.compatibility;
 
 import java.util.ArrayList;
