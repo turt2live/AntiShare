5253622cc4a2d8e455e4aac4c10eece8cf3726ea
Travis Ralston <travpc@gmail.com>
2012-07-28 23:09:53 -0600

    Can't claim these as mine

--- src/com/feildmaster/lib/configuration/PluginWrapper.java
+++ src/com/feildmaster/lib/configuration/PluginWrapper.java
@@ -1,34 +1,28 @@
-/*******************************************************************************
- * Copyright (c) 2012 turt2live (Travis Ralston).
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v2.1
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
- * Contributors:
- *     turt2live (Travis Ralston) - initial API and implementation
- ******************************************************************************/
 package com.feildmaster.lib.configuration;
 
 public class PluginWrapper extends org.bukkit.plugin.java.JavaPlugin {
-    private EnhancedConfiguration config;
+	private EnhancedConfiguration config;
 
-    public EnhancedConfiguration getConfig() {
-        if(config == null) {
-            config = new EnhancedConfiguration(this);
-        }
-        return config;
-    }
+	@Override
+	public EnhancedConfiguration getConfig(){
+		if(config == null){
+			config = new EnhancedConfiguration(this);
+		}
+		return config;
+	}
 
-    public void reloadConfig() {
-        getConfig().load();
-    }
+	@Override
+	public void reloadConfig(){
+		getConfig().load();
+	}
 
-    public void saveConfig() {
-        getConfig().save();
-    }
+	@Override
+	public void saveConfig(){
+		getConfig().save();
+	}
 
-    public void saveDefaultConfig() {
-        getConfig().saveDefaults();
-    }
+	@Override
+	public void saveDefaultConfig(){
+		getConfig().saveDefaults();
+	}
 }
