8a1384fe890897bbbb5356e7c30b0ae876693af6
Travis Ralston <travpc@gmail.com>
2012-09-12 18:47:14 -0600

    Reformat

--- src/com/turt2live/antishare/regions/RegionWall.java
+++ src/com/turt2live/antishare/regions/RegionWall.java
@@ -6,7 +6,7 @@
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * Contributors:
- *     turt2live (Travis Ralston) - initial API and implementation
+ * turt2live (Travis Ralston) - initial API and implementation
  ******************************************************************************/
 package com.turt2live.antishare.regions;
 
