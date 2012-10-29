91fe5b982e3124b288fa1dd9d1ba72c60223b1af
Travis Ralston <travpc@gmail.com>
2012-10-28 18:02:03 -0600

    Eclipse needs to be quiet sometimes.

--- src/com/turt2live/antishare/lib/patpeter/sqllibrary/Database.java
+++ src/com/turt2live/antishare/lib/patpeter/sqllibrary/Database.java
@@ -2,8 +2,8 @@ package com.turt2live.antishare.lib.patpeter.sqllibrary;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 /**
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
  * Abstract superclass for all subclass database files.
  * 
  * Date Created: 2011-08-26 19:08
+ * 
  * @author PatPeter
  */
 public abstract class Database {
@@ -19,271 +20,271 @@ public abstract class Database {
 	protected final String DATABASE_PREFIX;
 	protected boolean connected;
 	protected Connection connection;
-	
+
 	// http://dev.mysql.com/doc/refman/5.6/en/sql-syntax.html
 	// http://sqlite.org/lang.html
-	protected enum Statements {
+	protected enum Statements{
 		SELECT, INSERT, UPDATE, DELETE, DO, REPLACE, LOAD, HANDLER, CALL, // Data manipulation statements
-		CREATE, ALTER, DROP, TRUNCATE, RENAME,  // Data definition statements
-		
+		CREATE, ALTER, DROP, TRUNCATE, RENAME, // Data definition statements
+
 		RELEASE,
-		
+
 		// MySQL-specific
 		START, COMMIT, SAVEPOINT, ROLLBACK, LOCK, UNLOCK, // MySQL Transactional and Locking Statements
 		PREPARE, EXECUTE, DEALLOCATE, // Prepared Statements
 		SET, SHOW, // Database Administration
 		DESCRIBE, EXPLAIN, HELP, USE, // Utility Statements
-		
+
 		// SQLite-specific
 		ANALYZE, ATTACH, BEGIN, DETACH, END, INDEXED, ON, PRAGMA, REINDEX, VACUUM
 	}
-	
+
 	public int lastUpdate;
-	
+
 	/*
 	 *  MySQL, SQLite
 	 */
-	public Database(Logger log, String prefix, String dp) {
+	public Database(Logger log, String prefix, String dp){
 		this.log = log;
 		this.PREFIX = prefix;
 		this.DATABASE_PREFIX = dp;
 		this.connected = false;
 		this.connection = null;
 	}
-	
+
 	/**
 	 * <b>writeInfo</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Writes information to the console.
+	 * &nbsp;&nbsp;Writes information to the console. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @param message the <a href="http://download.oracle.com/javase/6/docs/api/java/lang/String.html">String</a>
-	 * of content to write to the console.
+	 *            of content to write to the console.
 	 */
-	protected String prefix(String message) {
+	protected String prefix(String message){
 		return this.PREFIX + this.DATABASE_PREFIX + message;
 	}
-	
+
 	/**
 	 * <b>writeInfo</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Writes information to the console.
+	 * &nbsp;&nbsp;Writes information to the console. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @param toWrite the <a href="http://download.oracle.com/javase/6/docs/api/java/lang/String.html">String</a>
-	 * of content to write to the console.
+	 *            of content to write to the console.
 	 */
-	protected void writeInfo(String toWrite) {
-		if (toWrite != null) {
+	protected void writeInfo(String toWrite){
+		if(toWrite != null){
 			this.log.info(prefix(toWrite));
 		}
 	}
-	
+
 	/**
 	 * <b>writeError</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Writes either errors or warnings to the console.
+	 * &nbsp;&nbsp;Writes either errors or warnings to the console. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @param toWrite the <a href="http://download.oracle.com/javase/6/docs/api/java/lang/String.html">String</a>
-	 * written to the console.
+	 *            written to the console.
 	 * @param severe whether console output should appear as an error or warning.
 	 */
-	protected void writeError(String toWrite, boolean severe) {
-		if (toWrite != null) {
-			if (severe) {
+	protected void writeError(String toWrite, boolean severe){
+		if(toWrite != null){
+			if(severe){
 				this.log.severe(prefix(toWrite));
-			} else {
+			}else{
 				this.log.warning(prefix(toWrite));
 			}
 		}
 	}
-	
+
 	/**
 	 * <b>initialize</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Used to check whether the class for the SQL engine is installed.
-	 * <br>
+	 * &nbsp;&nbsp;Used to check whether the class for the SQL engine is installed. <br>
 	 * <br>
 	 */
 	abstract boolean initialize();
-	
+
 	/**
 	 * <b>open</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Opens a connection with the database.
+	 * &nbsp;&nbsp;Opens a connection with the database. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @return the success of the method.
 	 */
 	abstract Connection open() throws SQLException;
-	
+
 	/**
 	 * <b>close</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Closes a connection with the database.
+	 * &nbsp;&nbsp;Closes a connection with the database. <br>
 	 * <br>
-	 * <br>
+	 * 
+	 * @return true on successful close
 	 */
 	//abstract boolean close();
-	public boolean close() {
-		if (connection != null) {
-			try {
+	public boolean close(){
+		if(connection != null){
+			try{
 				connection.close();
 				return true;
-			} catch (SQLException e) {
-				this.writeError("Could not close connection, SQLException: " + e.getMessage(), true);
+			}catch(SQLException e){
+				writeError("Could not close connection, SQLException: " + e.getMessage(), true);
 				return false;
 			}
-		} else {
-			this.writeError("Could not close connection, it is null.", true);
+		}else{
+			writeError("Could not close connection, it is null.", true);
 			return false;
 		}
 	}
-	
+
 	/**
 	 * <b>getConnection</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Gets the connection variable 
+	 * &nbsp;&nbsp;Gets the connection variable <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @return the <a href="http://download.oracle.com/javase/6/docs/api/java/sql/Connection.html">Connection</a> variable.
 	 */
 	//abstract Connection getConnection();
-	public Connection getConnection() {
+	public Connection getConnection(){
 		return this.connection;
 	}
-	
+
 	/**
 	 * <b>checkConnection</b><br>
 	 * <br>
-	 * Checks the connection between Java and the database engine.
+	 * Checks the connection between Java and the database engine. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @return the status of the connection, true for up, false for down.
 	 */
 	//abstract boolean checkConnection();
-	public boolean checkConnection() {
-		if (connection != null)
+	public boolean checkConnection(){
+		if(connection != null)
 			return true;
 		return false;
 	}
-	
+
 	/**
 	 * <b>query</b><br>
-	 * &nbsp;&nbsp;Sends a query to the SQL database.
+	 * &nbsp;&nbsp;Sends a query to the SQL database. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @param query the SQL query to send to the database.
 	 * @return the table of results from the query.
 	 */
 	abstract ResultSet query(String query) throws SQLException;
-	
+
 	/**
 	 * <b>prepare</b><br>
-	 * &nbsp;&nbsp;Prepares to send a query to the database.
+	 * &nbsp;&nbsp;Prepares to send a query to the database. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @param query the SQL query to prepare to send to the database.
 	 * @return the prepared statement.
+	 * @throws SQLException thrown if the SQL fails
 	 */
 	//abstract PreparedStatement prepare(String query) throws SQLException;
-	public PreparedStatement prepare(String query) throws SQLException {
-        return connection.prepareStatement(query);
+	public PreparedStatement prepare(String query) throws SQLException{
+		return connection.prepareStatement(query);
 	}
-	
+
 	/**
 	 * <b>getStatement</b><br>
-	 * &nbsp;&nbsp;Determines the name of the statement and converts it into an enum.
-	 * <br>
+	 * &nbsp;&nbsp;Determines the name of the statement and converts it into an enum. <br>
 	 * <br>
 	 */
-	protected Statements getStatement(String query) throws SQLException {
+	protected Statements getStatement(String query) throws SQLException{
 		/*String query = rawQuery.trim();
 		
 		if (query.length() < 10)
 			throw new SQLException("Queries must be at least 10 characters long.");*/
-		
+
 		//try {
-		if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("SELECT"))
+		if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("SELECT"))
 			return Statements.SELECT;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("INSERT"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("INSERT"))
 			return Statements.INSERT;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("UPDATE"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("UPDATE"))
 			return Statements.UPDATE;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("DELETE"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("DELETE"))
 			return Statements.DELETE;
-		else if (query.length() > 1 && query.substring(0,2).equalsIgnoreCase("DO"))
+		else if(query.length() > 1 && query.substring(0, 2).equalsIgnoreCase("DO"))
 			return Statements.DO;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("REPLACE"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("REPLACE"))
 			return Statements.REPLACE;
-		else if (query.length() > 3 && query.substring(0,4).equalsIgnoreCase("LOAD"))
+		else if(query.length() > 3 && query.substring(0, 4).equalsIgnoreCase("LOAD"))
 			return Statements.LOAD;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("HANDLER"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("HANDLER"))
 			return Statements.HANDLER;
-		else if (query.length() > 3 && query.substring(0,4).equalsIgnoreCase("CALL"))
+		else if(query.length() > 3 && query.substring(0, 4).equalsIgnoreCase("CALL"))
 			return Statements.CALL;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("CREATE"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("CREATE"))
 			return Statements.CREATE;
-		else if (query.length() > 4 && query.substring(0,5).equalsIgnoreCase("ALTER"))
+		else if(query.length() > 4 && query.substring(0, 5).equalsIgnoreCase("ALTER"))
 			return Statements.ALTER;
-		else if (query.length() > 3 && query.substring(0,4).equalsIgnoreCase("DROP"))
+		else if(query.length() > 3 && query.substring(0, 4).equalsIgnoreCase("DROP"))
 			return Statements.DROP;
-		else if (query.length() > 7 && query.substring(0,8).equalsIgnoreCase("TRUNCATE"))
+		else if(query.length() > 7 && query.substring(0, 8).equalsIgnoreCase("TRUNCATE"))
 			return Statements.TRUNCATE;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("RENAME"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("RENAME"))
 			return Statements.RENAME;
-		else if (query.length() > 4 && query.substring(0,5).equalsIgnoreCase("START"))
+		else if(query.length() > 4 && query.substring(0, 5).equalsIgnoreCase("START"))
 			return Statements.START;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("COMMIT"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("COMMIT"))
 			return Statements.COMMIT;
-		else if (query.length() > 7 && query.substring(0,8).equalsIgnoreCase("ROLLBACK"))
+		else if(query.length() > 7 && query.substring(0, 8).equalsIgnoreCase("ROLLBACK"))
 			return Statements.ROLLBACK;
-		else if (query.length() > 8 && query.substring(0,9).equalsIgnoreCase("SAVEPOINT"))
+		else if(query.length() > 8 && query.substring(0, 9).equalsIgnoreCase("SAVEPOINT"))
 			return Statements.SAVEPOINT;
-		else if (query.length() > 3 && query.substring(0,4).equalsIgnoreCase("LOCK"))
+		else if(query.length() > 3 && query.substring(0, 4).equalsIgnoreCase("LOCK"))
 			return Statements.LOCK;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("UNLOCK"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("UNLOCK"))
 			return Statements.UNLOCK;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("PREPARE"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("PREPARE"))
 			return Statements.PREPARE;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("EXECUTE"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("EXECUTE"))
 			return Statements.EXECUTE;
-		else if (query.length() > 9 && query.substring(0,10).equalsIgnoreCase("DEALLOCATE"))
+		else if(query.length() > 9 && query.substring(0, 10).equalsIgnoreCase("DEALLOCATE"))
 			return Statements.DEALLOCATE;
-		else if (query.length() > 2 && query.substring(0,3).equalsIgnoreCase("SET"))
+		else if(query.length() > 2 && query.substring(0, 3).equalsIgnoreCase("SET"))
 			return Statements.SET;
-		else if (query.length() > 3 && query.substring(0,4).equalsIgnoreCase("SHOW"))
+		else if(query.length() > 3 && query.substring(0, 4).equalsIgnoreCase("SHOW"))
 			return Statements.SHOW;
-		else if (query.length() > 7 && query.substring(0,8).equalsIgnoreCase("DESCRIBE"))
+		else if(query.length() > 7 && query.substring(0, 8).equalsIgnoreCase("DESCRIBE"))
 			return Statements.DESCRIBE;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("EXPLAIN"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("EXPLAIN"))
 			return Statements.EXPLAIN;
-		else if (query.length() > 3 && query.substring(0,4).equalsIgnoreCase("HELP"))
+		else if(query.length() > 3 && query.substring(0, 4).equalsIgnoreCase("HELP"))
 			return Statements.HELP;
-		else if (query.length() > 2 && query.substring(0,3).equalsIgnoreCase("USE"))
+		else if(query.length() > 2 && query.substring(0, 3).equalsIgnoreCase("USE"))
 			return Statements.USE;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("ANALYSE"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("ANALYSE"))
 			return Statements.ANALYZE;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("ATTACH"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("ATTACH"))
 			return Statements.ATTACH;
-		else if (query.length() > 4 && query.substring(0,5).equalsIgnoreCase("BEGIN"))
+		else if(query.length() > 4 && query.substring(0, 5).equalsIgnoreCase("BEGIN"))
 			return Statements.BEGIN;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("DETACH"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("DETACH"))
 			return Statements.DETACH;
-		else if (query.length() > 2 && query.substring(0,3).equalsIgnoreCase("END"))
+		else if(query.length() > 2 && query.substring(0, 3).equalsIgnoreCase("END"))
 			return Statements.END;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("INDEXED"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("INDEXED"))
 			return Statements.INDEXED;
-		else if (query.length() > 1 && query.substring(0,2).equalsIgnoreCase("ON"))
+		else if(query.length() > 1 && query.substring(0, 2).equalsIgnoreCase("ON"))
 			return Statements.ON;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("PRAGMA"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("PRAGMA"))
 			return Statements.PRAGMA;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("REINDEX"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("REINDEX"))
 			return Statements.REINDEX;
-		else if (query.length() > 6 && query.substring(0,7).equalsIgnoreCase("RELEASE"))
+		else if(query.length() > 6 && query.substring(0, 7).equalsIgnoreCase("RELEASE"))
 			return Statements.RELEASE;
-		else if (query.length() > 5 && query.substring(0,6).equalsIgnoreCase("VACUUM"))
+		else if(query.length() > 5 && query.substring(0, 6).equalsIgnoreCase("VACUUM"))
 			return Statements.VACUUM;
 		else
 			throw new SQLException("Unknown statement \"" + query + "\".");
@@ -291,37 +292,37 @@ public abstract class Database {
 		//	throw new SQLException("Query not long enough: \"" + query + "\".");
 		//}
 	}
-	
+
 	/**
 	 * <b>createTable</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Creates a table in the database based on a specified query.
+	 * &nbsp;&nbsp;Creates a table in the database based on a specified query. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @param query the SQL query for creating a table.
 	 * @return the success of the method.
-	 * @throws SQLException 
+	 * @throws SQLException
 	 */
 	abstract boolean createTable(String query);
-	
+
 	/**
 	 * <b>checkTable</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Checks a table in a database based on the table's name.
+	 * &nbsp;&nbsp;Checks a table in a database based on the table's name. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @param table name of the table to check.
 	 * @return success of the method.
-	 * @throws SQLException 
+	 * @throws SQLException
 	 */
 	abstract boolean checkTable(String table);
-	
+
 	/**
 	 * <b>wipeTable</b><br>
 	 * <br>
-	 * &nbsp;&nbsp;Wipes a table given its name.
+	 * &nbsp;&nbsp;Wipes a table given its name. <br>
 	 * <br>
-	 * <br>
+	 * 
 	 * @param table name of the table to wipe.
 	 * @return success of the method.
 	 */
--- src/com/turt2live/antishare/lib/patpeter/sqllibrary/DatabaseConfig.java
+++ src/com/turt2live/antishare/lib/patpeter/sqllibrary/DatabaseConfig.java
@@ -1,18 +1,18 @@
 /************************************************************************
- * This file is part of SQLibrary.									
- *																		
+ * This file is part of SQLibrary.
+ * 
  * SQLibrary is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by	
- * the Free Software Foundation, either version 3 of the License, or		
- * (at your option) any later version.									
- *																		
- * SQLibrary is distributed in the hope that it will be useful,	
- * but WITHOUT ANY WARRANTY; without even the implied warranty of		
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			
- * GNU General Public License for more details.							
- *																		
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SQLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Public License
- * along with SQLibrary.  If not, see <http://www.gnu.org/licenses/>.
+ * along with SQLibrary. If not, see <http://www.gnu.org/licenses/>.
  ************************************************************************/
 package com.turt2live.antishare.lib.patpeter.sqllibrary;
 
@@ -31,7 +31,7 @@ public class DatabaseConfig {
 	private DatabaseType type;
 	private Logger log;
 
-	public enum DatabaseType {
+	public enum DatabaseType{
 		MYSQL,
 		SQLITE,
 		MICROSOFTSQL,
@@ -41,7 +41,7 @@ public class DatabaseConfig {
 		ALL;
 	}
 
-	public enum Parameter {
+	public enum Parameter{
 		HOSTNAME(DatabaseType.MYSQL),
 		USER(DatabaseType.MYSQL),
 		PASSWORD(DatabaseType.MYSQL),
@@ -56,35 +56,35 @@ public class DatabaseConfig {
 		/**
 		 * 
 		 */
-		private Parameter(DatabaseType... type) {
-			for (int i = 0; i < type.length; i++) {
+		private Parameter(DatabaseType... type){
+			for(int i = 0; i < type.length; i++){
 				dbTypes.add(type[i]);
 				updateCount(type[i]);
 			}
 
 		}
 
-		public boolean validParam(DatabaseType toCheck) {
-			if (dbTypes.contains(DatabaseType.ALL))
+		public boolean validParam(DatabaseType toCheck){
+			if(dbTypes.contains(DatabaseType.ALL))
 				return true;
-			if (dbTypes.contains(toCheck))
+			if(dbTypes.contains(toCheck))
 				return true;
 			return false;
 
 		}
 
-		private static void updateCount(DatabaseType type) {
-			if (count == null)
+		private static void updateCount(DatabaseType type){
+			if(count == null)
 				count = new EnumMap<DatabaseType, Integer>(DatabaseType.class);
 			Integer nb = count.get(type);
-			if (nb == null)
+			if(nb == null)
 				nb = 1;
 			else
 				nb++;
 			count.put(type, nb);
 		}
 
-		public static int getCount(DatabaseType type) {
+		public static int getCount(DatabaseType type){
 			int nb = count.get(DatabaseType.ALL) + count.get(type);
 			return nb;
 		}
@@ -93,9 +93,10 @@ public class DatabaseConfig {
 	/**
 	 * @param type
 	 *            the type to set
+	 * @throws IllegalArgumentException thrown if the database type is "ALL"
 	 */
-	public void setType(DatabaseType type) throws IllegalArgumentException {
-		if (type == DatabaseType.ALL)
+	public void setType(DatabaseType type) throws IllegalArgumentException{
+		if(type == DatabaseType.ALL)
 			throw new IllegalArgumentException("You can't set your database type to ALL");
 		this.type = type;
 	}
@@ -104,29 +105,29 @@ public class DatabaseConfig {
 	 * @param log
 	 *            the log to set
 	 */
-	public void setLog(Logger log) {
+	public void setLog(Logger log){
 		this.log = log;
 	}
 
 	/**
 	 * @return the type
 	 */
-	public DatabaseType getType() {
+	public DatabaseType getType(){
 		return type;
 	}
 
 	/**
 	 * @return the log
 	 */
-	public Logger getLog() {
+	public Logger getLog(){
 		return log;
 	}
 
 	public DatabaseConfig setParameter(Parameter param, String value) throws NullPointerException,
-			InvalidConfiguration {
-		if (this.type == null)
+			InvalidConfiguration{
+		if(this.type == null)
 			throw new NullPointerException("You must set the type of the database first");
-		if (!param.validParam(type))
+		if(!param.validParam(type))
 			throw new InvalidConfiguration(param.toString()
 					+ " is invalid for a database type of : " + type.toString());
 		config.put(param, value);
@@ -134,12 +135,12 @@ public class DatabaseConfig {
 
 	}
 
-	public String getParameter(Parameter param) {
+	public String getParameter(Parameter param){
 		return config.get(param);
 	}
 
-	public boolean isValid() throws InvalidConfiguration {
-		if (log == null)
+	public boolean isValid() throws InvalidConfiguration{
+		if(log == null)
 			throw new InvalidConfiguration("You need to set the logger.");
 		return config.size() == Parameter.getCount(type);
 	}
