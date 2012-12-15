e7f00229e2ad9d66ff97140c55a52bfc4a4ea57b
Travis Ralston <travpc@gmail.com>
2012-12-14 21:05:41 -0700

    No need to waste resources

--- src/main/java/com/turt2live/antishare/util/generic/ConflictThread.java
+++ src/main/java/com/turt2live/antishare/util/generic/ConflictThread.java
@@ -25,54 +25,54 @@ public class ConflictThread implements Runnable {
 		Plugin mv = plugin.getServer().getPluginManager().getPlugin("Multiverse");
 		if(mv != null){
 			if(plugin.getConfig().getBoolean("handled-actions.gamemode-inventories")){
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
-				AntiShare.getInstance().log("* You have Multiverse installed and GameMode Inventories enabled!                   *", Level.WARNING);
-				AntiShare.getInstance().log("* Although bugs are not expected to occur, they are still possible. If unexpected   *", Level.WARNING);
-				AntiShare.getInstance().log("* inventory bugs occur, or world bugs, check your configurations in both plugins.   *", Level.WARNING);
-				AntiShare.getInstance().log("* Inventory loss bugs have been reported and can be solved with correct configs.    *", Level.WARNING);
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
+				plugin.log("* You have Multiverse installed and GameMode Inventories enabled!                   *", Level.WARNING);
+				plugin.log("* Although bugs are not expected to occur, they are still possible. If unexpected   *", Level.WARNING);
+				plugin.log("* inventory bugs occur, or world bugs, check your configurations in both plugins.   *", Level.WARNING);
+				plugin.log("* Inventory loss bugs have been reported and can be solved with correct configs.    *", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
 			}
 			if(plugin.getConfig().getBoolean("handled-actions.world-transfers")){
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
-				AntiShare.getInstance().log("* You have Multiverse installed and World Transfers enabled!                        *", Level.WARNING);
-				AntiShare.getInstance().log("* Although bugs are not expected to occur, they are still possible. If unexpected   *", Level.WARNING);
-				AntiShare.getInstance().log("* inventory bugs occur, or world bugs, check your configurations in both plugins.   *", Level.WARNING);
-				AntiShare.getInstance().log("* Inventory loss bugs have been reported and can be solved with correct configs.    *", Level.WARNING);
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
+				plugin.log("* You have Multiverse installed and World Transfers enabled!                        *", Level.WARNING);
+				plugin.log("* Although bugs are not expected to occur, they are still possible. If unexpected   *", Level.WARNING);
+				plugin.log("* inventory bugs occur, or world bugs, check your configurations in both plugins.   *", Level.WARNING);
+				plugin.log("* Inventory loss bugs have been reported and can be solved with correct configs.    *", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
 			}
 		}
 		Plugin mvinv = plugin.getServer().getPluginManager().getPlugin("Multiverse-Inventories");
 		if(mvinv != null){
 			if(plugin.getConfig().getBoolean("handled-actions.gamemode-inventories")){
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
-				AntiShare.getInstance().log("* You have Multiverse-Inventories and GameMode Inventories enabled!                 *", Level.WARNING);
-				AntiShare.getInstance().log("* This may cause issues or unexpected results when both plugins handle inventories. *", Level.WARNING);
-				AntiShare.getInstance().log("* I suggest you edit my configuration to allow Multiverse to do it's job            *", Level.WARNING);
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
+				plugin.log("* You have Multiverse-Inventories and GameMode Inventories enabled!                 *", Level.WARNING);
+				plugin.log("* This may cause issues or unexpected results when both plugins handle inventories. *", Level.WARNING);
+				plugin.log("* I suggest you edit my configuration to allow Multiverse to do it's job            *", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
 			}
 			if(plugin.getConfig().getBoolean("handled-actions.world-transfers")){
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
-				AntiShare.getInstance().log("* You have Multiverse-Inventories and World Inventories enabled!                    *", Level.WARNING);
-				AntiShare.getInstance().log("* This may cause issues or unexpected results when both plugins handle inventories. *", Level.WARNING);
-				AntiShare.getInstance().log("* I suggest you edit my configuration to allow Multiverse to do it's job            *", Level.WARNING);
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
+				plugin.log("* You have Multiverse-Inventories and World Inventories enabled!                    *", Level.WARNING);
+				plugin.log("* This may cause issues or unexpected results when both plugins handle inventories. *", Level.WARNING);
+				plugin.log("* I suggest you edit my configuration to allow Multiverse to do it's job            *", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
 			}
 		}
 		Plugin multiinv = plugin.getServer().getPluginManager().getPlugin("MultiInv");
 		if(multiinv != null){
 			if(plugin.getConfig().getBoolean("handled-actions.gamemode-inventories")){
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
-				AntiShare.getInstance().log("* You have MultiInv and GameMode Inventories enabled!                               *", Level.WARNING);
-				AntiShare.getInstance().log("* This may cause issues or unexpected results when both plugins handle inventories. *", Level.WARNING);
-				AntiShare.getInstance().log("* I suggest you edit my configuration to allow MultiInv to do it's job              *", Level.WARNING);
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
+				plugin.log("* You have MultiInv and GameMode Inventories enabled!                               *", Level.WARNING);
+				plugin.log("* This may cause issues or unexpected results when both plugins handle inventories. *", Level.WARNING);
+				plugin.log("* I suggest you edit my configuration to allow MultiInv to do it's job              *", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
 			}
 			if(plugin.getConfig().getBoolean("handled-actions.world-transfers")){
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
-				AntiShare.getInstance().log("* You have MultiInv and World Inventories enabled!                                  *", Level.WARNING);
-				AntiShare.getInstance().log("* This may cause issues or unexpected results when both plugins handle inventories. *", Level.WARNING);
-				AntiShare.getInstance().log("* I suggest you edit my configuration to allow MultiInv to do it's job              *", Level.WARNING);
-				AntiShare.getInstance().log("*************************************************************************************", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
+				plugin.log("* You have MultiInv and World Inventories enabled!                                  *", Level.WARNING);
+				plugin.log("* This may cause issues or unexpected results when both plugins handle inventories. *", Level.WARNING);
+				plugin.log("* I suggest you edit my configuration to allow MultiInv to do it's job              *", Level.WARNING);
+				plugin.log("*************************************************************************************", Level.WARNING);
 			}
 		}
 	}
