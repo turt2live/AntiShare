fbfe38a6ebd36226b2140cc8cce4614ab0a4a13c
Travis Ralston <travpc@gmail.com>
2013-02-12 19:10:02 -0700

    Will work on after release

--- src/main/java/com/turt2live/antishare/pail/PailPane.java
+++ src/main/java/com/turt2live/antishare/pail/PailPane.java
@@ -1,5 +1,6 @@
 package com.turt2live.antishare.pail;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -13,24 +14,41 @@ import com.turt2live.antishare.AntiShare;
 public class PailPane extends JPanel {
 
 	private static final long serialVersionUID = 3497861298959219768L;
-
+	private static Pail pail;
 	private AntiShare plugin = AntiShare.getInstance();
-	private Pail pail;
 
 	public PailPane(Pail pail){
-		this.pail = pail;
+		PailPane.pail = pail;
+		//		ConfigPane config = new ConfigPane();
+		//		ListPane blockLists = new ListPane();
+		//		
+		//		setLayout(new BorderLayout(0, 0));
+		//
+		//		JTabbedPane tabs = new JTabbedPane();
+		//		tabs.setTabLayoutPolicy(javax.swing.JTabbedPane.SCROLL_TAB_LAYOUT);
+		//		tabs.setTabPlacement(JTabbedPane.LEFT);
+		//
+		//		tabs.addTab(translate("Configuration"), config);
+		//		tabs.addTab(translate("Block/Track Lists"), blockLists);
+		//
+		//		add(tabs, BorderLayout.CENTER);
+		//		validate();
 	}
 
-	@Override
 	public void paint(Graphics g){
 		super.paint(g);
 
 		BufferedImage image = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2 = image.createGraphics();
-		g2.drawString("AntiShare version " + plugin.getDescription().getVersion() + " build " + plugin.getBuild(), 10, 10);
-		g2.drawString("Pail version " + this.pail.getDescription().getVersion(), 10, 40);
+		g2.setColor(Color.BLACK);
+		g2.drawString("Settings Coming Soon", 10, 30);
+		g2.drawString("AntiShare version " + plugin.getDescription().getVersion() + " build " + plugin.getBuild(), 10, 50);
 
 		g.drawImage(image, 0, 0, null);
+	}
+
+	public static String translate(String english){
+		return pail.translate(english);
 	}
 
 }
