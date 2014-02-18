78400a0982da7313e7b4d2ae4bd7db3db8b8887a
turt2live <travpc@gmail.com>
2014-02-18 00:02:01 -0700

    Remove bombing test

--- src/main/java/com/turt2live/antishare/bukkit/EngineListener.java
+++ src/main/java/com/turt2live/antishare/bukkit/EngineListener.java
@@ -4,7 +4,6 @@ import com.turt2live.antishare.ASLocation;
 import com.turt2live.antishare.ASUtils;
 import com.turt2live.antishare.BlockType;
 import com.turt2live.antishare.engine.Engine;
-import com.turt2live.antishare.io.flatfile.FileBlockStore;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -59,38 +58,6 @@ public class EngineListener implements Listener {
                             .getBlockManager().getBlockType(BukkitUtils.toLocation(block.getLocation()));
                     player.sendMessage(ChatColor.YELLOW + "Block Type: " + ChatColor.GOLD + type.name());
                 }
-            } else if (player.getItemInHand().getType() == Material.DIAMOND) {
-                event.setCancelled(true);
-                FileBlockStore store = (FileBlockStore) engine.getEngine(player.getWorld().getName()).getBlockManager().getStore(BukkitUtils.toLocation(player.getLocation()));
-                store.clear(); // Remove any bad data
-                int[] header = store.getHeader();
-
-                System.out.println("filling");
-                for (int x = 0; x < header[3]; x++) {
-                    for (int y = 0; y < header[3]; y++) {
-                        for (int z = 0; z < header[3]; z++) {
-                            store.setType((header[0] * header[3]) + x,
-                                    (header[1] * header[3]) + y,
-                                    (header[2] * header[3]) + z,
-                                    BlockType.SPECTATOR);
-                        }
-                    }
-                }
-
-                System.out.println("save");
-                long save = System.nanoTime();
-                store.save();
-                System.out.println("load");
-                long load = System.nanoTime();
-                store.load();
-                long done = System.nanoTime();
-
-                long tsave = load - save;
-                long tload = done - load;
-                long tt = done - save;
-                System.out.println("Save: " + tsave + " ns (" + (tsave / 1000000) + "ms)");
-                System.out.println("Load: " + tload + " ns (" + (tload / 1000000) + "ms)");
-                System.out.println("Total: " + tt + " ns (" + (tt / 1000000) + "ms)");
             }
         }
     }
