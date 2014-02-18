946bd42110caaf6fd2d12fcd93405085d40086e3
turt2live <travpc@gmail.com>
2014-02-18 02:04:09 -0700

    Add some settings into this config.yml

--- src/main/java/com/turt2live/antishare/bukkit/AntiShare.java
+++ src/main/java/com/turt2live/antishare/bukkit/AntiShare.java
@@ -18,19 +18,40 @@ public class AntiShare extends JavaPlugin implements com.turt2live.antishare.eng
 
     private static AntiShare instance;
     private File dataFolder;
+    private int blockSize;
 
     @Override
     public void onLoad() {
         instance = this;
 
         dataFolder = new File(getDataFolder(), "data");
-
         if (!dataFolder.exists()) dataFolder.mkdirs();
     }
 
     @Override
     public void onEnable() {
         Engine.getInstance().addListener(this);
+        Engine.getInstance().setLogger(this.getLogger());
+
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(false);
+
+        // load engine variables
+        blockSize = getConfig().getInt("caching.block-size", 256);
+        long cacheMax = getConfig().getLong("caching.cache-expiration", 120000);
+        long cacheInterval = getConfig().getLong("caching.cache-timer-interval", 60000);
+        long periodicSave = getConfig().getLong("caching.periodic-save", 0);
+
+        // Validate engine variables
+        if (blockSize <= 0) blockSize = 256;
+        if (cacheMax <= 0) cacheMax = 120000;
+        if (cacheInterval <= 0) cacheInterval = 60000;
+
+        // Setup engine
+        Engine.getInstance().setCacheMaximum(cacheMax);
+        Engine.getInstance().setCacheIncrement(cacheInterval);
+        Engine.getInstance().setSaveInterval(periodicSave);
 
         // Setup listeners
         getServer().getPluginManager().registerEvents(new EngineListener(), this);
@@ -46,6 +67,7 @@ public class AntiShare extends JavaPlugin implements com.turt2live.antishare.eng
         // Save everything
         Engine.getInstance().prepareShutdown();
 
+        // Cleanup
         getServer().getScheduler().cancelTasks(this);
     }
 
@@ -55,7 +77,7 @@ public class AntiShare extends JavaPlugin implements com.turt2live.antishare.eng
 
         if (!storeLocation.exists()) storeLocation.mkdirs();
 
-        engine.setBlockManager(new FileBlockManager(2048, storeLocation)); // TODO: Configuration
+        engine.setBlockManager(new FileBlockManager(blockSize, storeLocation)); // TODO: Configuration (type)
 
         for (ASGameMode gameMode : ASGameMode.values()) {
             engine.setTrackedBlocks(gameMode, new DummyBlockTracker()); // TODO: Debug code
@@ -63,6 +85,15 @@ public class AntiShare extends JavaPlugin implements com.turt2live.antishare.eng
 
         // Start the engine
         engine.getBlockManager().loadAll();
+    }
+
+    /**
+     * Gets the block size this AntiShare plugin is using
+     *
+     * @return the block size
+     */
+    public int getBlockSize() {
+        return blockSize;
     }
 
     @Override
