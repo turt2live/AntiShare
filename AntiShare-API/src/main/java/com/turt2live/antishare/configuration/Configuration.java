836c6c577811d5ec739bbf260d3d8461a2395a58
turt2live <travpc@gmail.com>
2014-04-23 22:10:53 -0600

    Part 1.5 of configuration stuff

---
+++ AntiShare-API/src/main/java/com/turt2live/antishare/configuration/Configuration.java
@@ -0,0 +1,12 @@
+package com.turt2live.antishare.configuration;
+
+/**
+ * An AntiShare configuration. This is used for configuring various
+ * control mechanisms of the plugin.
+ * @author turt2live
+ */
+public abstract class Configuration {
+
+
+
+}
