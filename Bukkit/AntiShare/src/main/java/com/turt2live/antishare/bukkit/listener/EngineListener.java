cd1d9f409832a7ec922539f909be874dda27fe8b
turt2live <travpc@gmail.com>
2014-05-03 22:13:39 -0600

    Handle falling blocks

--- Bukkit/AntiShare/src/main/java/com/turt2live/antishare/bukkit/listener/EngineListener.java
+++ Bukkit/AntiShare/src/main/java/com/turt2live/antishare/bukkit/listener/EngineListener.java
@@ -2,24 +2,31 @@ package com.turt2live.antishare.bukkit.listener;
 
 import com.turt2live.antishare.*;
 import com.turt2live.antishare.bukkit.AntiShare;
+import com.turt2live.antishare.bukkit.BukkitUtils;
 import com.turt2live.antishare.bukkit.impl.BukkitBlock;
 import com.turt2live.antishare.bukkit.impl.BukkitPlayer;
 import com.turt2live.antishare.bukkit.lang.Lang;
 import com.turt2live.antishare.bukkit.lang.LangBuilder;
 import com.turt2live.antishare.engine.Engine;
+import com.turt2live.antishare.engine.WorldEngine;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 
 /**
  * AntiShare Bukkit Listener for the AntiShare Engine. This listener will only
@@ -49,6 +56,33 @@ public class EngineListener implements Listener {
             String blockType = AntiShare.getInstance().getMaterialProvider().getPlayerFriendlyName(event.getBlock());
             player.sendMessage(new LangBuilder(Lang.getInstance().getFormat(Lang.NAUGHTY_PLACE)).withPrefix().setReplacement(LangBuilder.SELECTOR_VARIABLE, blockType).build());
             alert(Lang.NAUGHTY_ADMIN_PLACE, event.getPlayer(), event.getBlock());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onFallingBlock(EntityChangeBlockEvent event) {
+        Entity eventEntity = event.getEntity();
+        WorldEngine engine = this.engine.getEngine(eventEntity.getWorld().getName());
+        ASLocation location = BukkitUtils.toLocation(event.getBlock().getLocation());
+        if (eventEntity.getType() == EntityType.FALLING_BLOCK) {
+            FallingBlock entity = (FallingBlock) eventEntity;
+            if (event.getTo() == Material.AIR) {
+                // Spawning
+                BlockType current = engine.getBlockManager().getBlockType(location);
+                if (Engine.getInstance().isPhysicsBreakAsGamemode() && current == BlockType.CREATIVE) {
+                    entity.setDropItem(false); // For if it hits something
+                }
+                entity.setMetadata("ANTISHARE_SAND", new FixedMetadataValue(AntiShare.getInstance(), current));
+            } else {
+                // Landing
+                if (entity.hasMetadata("ANTISHARE_SAND")) {
+                    try {
+                        BlockType previous = (BlockType) entity.getMetadata("ANTISHARE_SAND").get(0).value();
+                        engine.getBlockManager().setBlockType(location, previous);
+                    } catch (Exception e) { // I'm lazy
+                    }
+                }
+            }
         }
     }
 
