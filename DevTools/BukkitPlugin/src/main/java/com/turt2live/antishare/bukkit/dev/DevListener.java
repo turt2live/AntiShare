702f0f09b96efcf537e091be1ca69309de437c29
turt2live <travpc@gmail.com>
2014-05-04 22:42:57 -0600

    Add a chest type check tool

---
+++ DevTools/BukkitPlugin/src/main/java/com/turt2live/antishare/bukkit/dev/DevListener.java
@@ -0,0 +1,30 @@
+package com.turt2live.antishare.bukkit.dev;
+
+import com.turt2live.antishare.bukkit.impl.BukkitBlock;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class DevListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (player.getItemInHand() != null) {
+
+            // Chest type check
+            if (player.getItemInHand().getType() == Material.COMPASS) {
+                Block clicked = event.getClickedBlock();
+                if (clicked != null) {
+                    player.sendMessage(new BukkitBlock(clicked).getChestType().name());
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+}
