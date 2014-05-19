0e26a9865dc62196cdbccb3b91e52c6946b8c009
turt2live <travpc@gmail.com>
2014-05-19 00:43:04 -0600

    Cleanup

--- AntiShare-API/src/main/java/com/turt2live/antishare/object/pattern/IronGolemMobPattern.java
+++ AntiShare-API/src/main/java/com/turt2live/antishare/object/pattern/IronGolemMobPattern.java
@@ -30,7 +30,7 @@ import java.util.List;
  * @author turt2live
  */
 // TODO: Unit test
-public abstract class IronGolemMobPattern implements BlockPattern, MobPattern{
+public abstract class IronGolemMobPattern implements BlockPattern, MobPattern {
 
     protected enum IronGolemBlock {
         PUMPKIN,
